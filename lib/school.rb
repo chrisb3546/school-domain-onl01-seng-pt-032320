9ad5a3fb5001ef0ee2f2e8c2fb18312dfe68d237
chris brady <chrisb3546@yahoo.com>
2020-04-09 14:39:09 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,13 +2,14 @@
 class School
  
  attr_reader :name, :roster
+  
   def initialize(name)
    @name = name
    @roster = {}
  end 
  
  def add_student(grade, name)
-   
+   @roster[grade] = [student]
  
  end 
  
